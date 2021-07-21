efcde18d4d845fed67f7bf023439a541e596a53c
kevinlee075 <kevinlee075@gmail.com>
2021-07-20 21:33:40 -0500

    Update ps6.py

--- ps6.py
+++ ps6.py
@@ -275,7 +275,18 @@ class CiphertextMessage(Message):
                 
         return (best_shift, perfect_decrypted_text)                            #form them as a tuple based the requirement
         
-                
+def decrypt_story():
+    '''
+    Now that you have all the pieces to the puzzle,
+    please use them to decode the file story.txt.
+    
+    The file ps6.py contains a helper function get_story_string()
+    that returns the encrypted version of the story as a string.
+    
+    Create a CiphertextMessage object using the story string and
+    use decrypt_message to return the appropriate shift value and
+    unencrypted story string.
+    '''                
             
 
 #Example test case (PlaintextMessage)
